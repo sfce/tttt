e3bfce55c27d8139f2f3a4040ac1365139c53392
sfce <sfce.waiting@gmail.com>
2013-12-11 22:00:38 +0800

    commit project 20131211

--- src/us/cmcc/sms/cleaner/fragment/BlackNumberFragment.java
+++ src/us/cmcc/sms/cleaner/fragment/BlackNumberFragment.java
@@ -1,11 +1,23 @@
 package us.cmcc.sms.cleaner.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import me.sfce.library.activity.BaseFragment;
+import us.cmcc.sms.cleaner.MyApplication;
 import us.cmcc.sms.cleaner.R;
+import us.cmcc.sms.cleaner.dao.BlackNumber;
+import us.cmcc.sms.cleaner.dao.BlackNumberDao;
+import us.cmcc.sms.cleaner.dao.DaoMaster;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,13 +25,45 @@ import us.cmcc.sms.cleaner.R;
  * Date: 13-12-8
  * Time: 下午8:03
  */
-public class BlackNumberFragment extends BaseFragment {
-    @Override
-    protected View onPrepareView(LayoutInflater inflater, Bundle savedInstanceState) {
+public class BlackNumberFragment extends ListFragment {
 
-        View view = inflater.inflate(R.layout.main_tab, null);
-        TextView textView = (TextView) view.findViewById(android.R.id.text1);
-        textView.setText("黑名单号码");
-        return view;
+    private BlackNumberDao dao;
+    private List<BlackNumber> blackNumbers;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_list, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView empty = (TextView) getView().findViewById(android.R.id.empty);
+        empty.setText("展示在这里的手机号的短信将被拦截");
+        DaoMaster.DevOpenHelper dbOpenHelper = new DaoMaster.DevOpenHelper(getActivity(), MyApplication.DB_NAME, null);
+        SQLiteDatabase db = dbOpenHelper.getReadableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        dao = daoMaster.newSession().getBlackNumberDao();
+        blackNumbers = dao.loadAll();
+        setListAdapter(new ArrayAdapter<BlackNumber>(getActivity(), android.R.layout.simple_list_item_1, blackNumbers));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        new AlertDialog.Builder(getActivity()).setItems(new String[]{"修改", "删除", "短信", "呼叫"}, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        }).create().show();
     }
 }
