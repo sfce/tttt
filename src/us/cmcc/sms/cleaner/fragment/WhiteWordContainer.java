82ee8934c2f3e384816962e22a18895754c13d14
yangfk <hunhaizhilian@126.com>
2013-12-12 23:52:43 +0800

    Signed-off-by: yangfk <hunhaizhilian@126.com>

---
+++ src/us/cmcc/sms/cleaner/fragment/WhiteWordContainer.java
@@ -0,0 +1,16 @@
+package us.cmcc.sms.cleaner.fragment;
+
+import me.sfce.library.activity.BaseContainerFragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sfce
+ * Date: 13-12-8
+ * Time: 下午7:26
+ */
+public class WhiteWordContainer extends BaseContainerFragment {
+    @Override
+    protected void init() {
+    	replaceFragment(new WhiteWordFragment(), uniqueTag(WhiteWordFragment.class), false);
+    }
+}
